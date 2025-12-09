c00a73f2824d7db0ac5d4c3b8e8fe06cc97c658d
montasia harris <mharris@my.yearupunited.com>
2025-12-09 10:32:01 -0500

    spring boot northwind

--- NorthwindTradersSpringboot/src/main/java/com/northwindtraders/NorthwindApplication.java
+++ NorthwindTradersSpringboot/src/main/java/com/northwindtraders/NorthwindApplication.java
@@ -1,4 +1,29 @@
 package com.northwindtraders;
 
-public class NorthwindApplication {
+
+import com.northwindtraders.data.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+public class NorthwindApplication implements CommandLineRunner {
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("=== Northwind Traders CLI ===");
+
+
+        System.out.println("Products in database:");
+        productDao.getAllProducts().forEach(product -> {
+            System.out.println(product.getProductId() + " - " + product.getProductName());
+        });
+
+
+    }
 }
+
